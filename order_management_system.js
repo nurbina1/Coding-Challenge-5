91d1901c2383155e1812edd1fe6a8de940214bcf
Abylkhair03 <nurbina@usf.edu>
2024-09-21 19:17:30 -0400

    Initialize orders array

---
+++ order_management_system.js
@@ -0,0 +1,39 @@
+let inventory = [
+{
+    name: "Dunkin Latte",
+    price: 10,
+    quantity: 20
+},
+{
+    name: "Cappuchino",
+    price: 5,
+    quantity: 25
+},
+{
+    name: "Matcha Latte",
+    price: 6,
+    quantity: 15
+},
+{
+    name: "Americano",
+    price: 4,
+    quantity: 12
+}
+];
+
+let orders = [
+{
+    costumerName: "John",
+    items: [
+        {name: "Americano", quantity: 1},
+        {name: "Matcha Latte", quantity: 2}],
+    status: "Pending"
+},
+{
+    costumerName: "AKMAL",
+    items:[
+        {name: "Cappuchino", quantity: 2}],
+    status: "Completed"
+    
+}
+];
